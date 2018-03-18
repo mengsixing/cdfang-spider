b8233d73590fa06a707347c575ff0ab692255b4e
尹珩力 <yinhengli@sina.com>
2018-03-18 23:02:41 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
-export default () => <div>Index!</div>
+import React from 'react'
+
+export default class extends React.Component {
+  static async getInitialProps({ req }) {
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
+    return { userAgent }
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World {this.props.userAgent}
+      </div>
+    )
+  }
+}

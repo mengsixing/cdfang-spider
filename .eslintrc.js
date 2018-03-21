aa3bc800dfa859324d265408e0cc70f1b31e018c
尹珩力 <yinhengli@sina.com>
2018-03-21 23:03:54 +0800

    import eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 8,
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": ["error", "always"],
+        "no-console":["warn", { allow: ["warn", "error"] }]
+    }
+};
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-import koa from 'koa'
+import koa from 'koa';
 
-import "babel-polyfill"
-const request = require('superagent')
-const cheerio = require('cheerio')
+import 'babel-polyfill';
+import request from 'superagent';
+import cheerio from 'cheerio';
+import mongoose from 'mongoose';
 
 const app = new koa();
 
 app.use(async (ctx) => {
-  const mongoose = require('mongoose');
-  mongoose.connect('mongodb://localhost/my_database');
-  var result = await new Promise((resolve,reject)=>{
-    request
-    .post('http://171.221.172.13:8888/lottery/accept/projectList')
-    .end((err, result) => {
-      var $ = cheerio.load(result.res.text);
-      var trList = [];
-      $('#_projectInfo>tr').each((i, tr) => {
-        var tdList = [];
-        $(tr).find('td').each((j, td) => {
-          tdList.push($(td).text());
-        });
-        trList.push(tdList);
-      });
-      // res.myData=trList;
-      resolve(trList);
-    });
-  })
-  ctx.body = result;
-})
+	mongoose.connect('mongodb://localhost/my_database');
+	var result = await new Promise((resolve) => {
+		request
+			.post('http://171.221.172.13:8888/lottery/accept/projectList')
+			.end((err, result) => {
+				var $ = cheerio.load(result.res.text);
+				var trList = [];
+				$('#_projectInfo>tr').each((i, tr) => {
+					var tdList = [];
+					$(tr).find('td').each((j, td) => {
+						tdList.push($(td).text());
+					});
+					trList.push(tdList);
+				});
+				// res.myData=trList;
+				resolve(trList);
+			});
+	});
+	ctx.body = result;
+});
 
 console.log('server run at4: localhost:3333');
-app.listen(3333)
+app.listen(3333);

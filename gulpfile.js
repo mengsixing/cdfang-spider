4b110be4437538e9f51c93c5b78cbbd8a8954c19
yhlben <yinhengli@sina.com>
2018-03-27 15:05:28 +0800

    fix a bug about npm run dev

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,15 @@ var gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('default', () =>
-  gulp.src('./nodeuii/**/*.js')
-    .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(gulp.dest('./dist/'))
+	gulp.src('./nodeuii/**/*.js')
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.pipe(gulp.dest('./dist/'))
 );
 
-
-if(!process.env.NODE_ENV=='production'){
-  gulp.watch('./nodeuii/*.js', ['default']);
+if(process.env.NODE_ENV!='production'){
+	gulp.watch('./nodeuii/*.js', ['default']);
 }
 
 

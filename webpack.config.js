89e032b9d5ca8c0162cd3a7219a68e4798bb4dd7
尹珩力 <yinhengli@sina.com>
2018-03-30 23:12:43 +0800

    add rimraf

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	},
 	output: {
 		path: __dirname+'/dist/client',
-		chunkFilename: '[name].bundle.js',
+		chunkFilename: '[name][hash].bundle.js',
 		filename: 'bundle.js'
 	},
 	module: {

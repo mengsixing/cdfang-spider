ef556aa8ae4526552bc9c1148ff22ca1a2096db9
尹珩力 <yinhengli@sina.com>
2018-04-01 17:10:50 +0800

    test husky

--- client/App.js
+++ client/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
 			this.props.appState.allData.replace(json);
 		});
 	}
-	
+	asdasda
 	render() {
 		var allData=this.props.appState.allData;
 		var areas=_.groupBy(allData,function(item){return item.area; } );

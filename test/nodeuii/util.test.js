b93a97c890d3f2d31b3c3805456869d02ff90f8d
yhlben <yinhengli@sina.com>
2018-07-24 20:16:06 +0800

    test ci2

--- test/nodeuii/util.test.js
+++ test/nodeuii/util.test.js
@@ -2,9 +2,12 @@ import * as util from '../../nodeuii/src/util.js';
 
 import chai from 'chai';
 var assert = chai.assert;
+var expect = chai.expect;
+
 
 describe('getCurrentQuarter测试', function () {
-  it('getCurrentQuarter测试对吗？', function () {
+  it('getCurrentQuarter返回参数长度是否正确？', function () {
     assert.typeOf(util.transformArray, 'function');
+    expect(util.transformArray([1,2,3,4,5,6,7,8,9,10,11,12])).to.have.lengthOf(12);
   });
 });

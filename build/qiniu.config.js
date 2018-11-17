ad5f87fc4e1aa024c41663cf2e41a43f3b7a6552
yhlben <yinhengliben@gmail.com>
2018-11-17 17:41:16 +0800

    fix: 修改七牛域名

--- build/qiniu.config.js
+++ build/qiniu.config.js
@@ -1,6 +1,6 @@
 // 我的七牛云存储信息，请勿使用！！
 module.exports = {
-	publicPath: 'http://pi33luk08.bkt.clouddn.com',
+	publicPath: 'http://cdn.yinhengli.com',
 	accessKey: 'usrU1J2-BTCqaODusj4JnOtW-SPGbw59hVUcFeHJ',
 	secretKey: 'Ff0Ggl7l8XK2Ysm33pWKfHF0IZWqdVL1uHVl6Mge',
 	bucket: 'mygithub',

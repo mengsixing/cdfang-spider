859553d6e9f090373a516010cd9ba824903eeb24
yhlben <yinhengliben@gmail.com>
2018-11-17 10:57:42 +0800

    docs: 修改服务器地址

--- client/config/config.js
+++ client/config/config.js
@@ -1,6 +1,6 @@
 var serverDomain = 'http://localhost:8082';
 if (process.env.NODE_ENV == 'production') {
-	serverDomain = 'http://yinhengli.com:8082';
+	serverDomain = 'http://47.107.177.146:8082';
 }
 
 var config = {

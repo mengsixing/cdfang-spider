64a42236e27eafdda6911cc171c0daee7f954073
yhlben <yinhengliben@gmail.com>
2019-01-06 14:07:25 +0800

    feat: 🎸 增加错误处理中间件

--- nodeuii/app.js
+++ nodeuii/app.js
@@ -1,12 +1,22 @@
+import 'babel-polyfill';
 import koa from 'koa';
 import serve from 'koa-static';
 import cors from 'koa2-cors';
-import 'babel-polyfill';
+import log4js from 'log4js';
 
+import ErrorHander from './middlewares/ErrorHander';
 import router from './src/router';
 import './src/schedule';
 
 const app = new koa();
+
+// 错误日志记录
+log4js.configure({
+	appenders: { globallog: { type: 'file', filename: './logs/globallog.log' } },
+	categories: { default: { appenders: ['globallog'], level: 'error' } }
+});
+const logger = log4js.getLogger('globallog');
+new ErrorHander().init(app, logger);
 
 // 允许跨域
 app.use(cors());
---
+++ nodeuii/middlewares/ErrorHander.js
@@ -0,0 +1,25 @@
+class ErrorHander {
+	init(app, logger) {
+		//捕获内部错误
+		app.use(async (ctx, next) => {
+			try {
+				await next();
+			} catch (e) {
+				logger.error(JSON.stringify(e));
+				ctx.status = 500;
+				ctx.body = '内部错误';
+			}
+		});
+		//捕获404错误
+		app.use(async (ctx, next) => {
+			await next();
+			if (ctx.status != 404) {
+				return;
+			} else {
+				ctx.body = '没有找到页面😰...';
+			}
+		});
+	}
+}
+
+export default ErrorHander;

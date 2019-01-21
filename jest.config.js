8f74051136808b98ab3f8831aa8a3f2b1e638025
yhlben <yinhengliben@gmail.com>
2019-01-21 19:55:52 +0800

    test: 修改测试用例

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  setupFiles: ['./__test__/setup.js', 'jest-canvas-mock'],
+  setupFiles: ['./__tests__/setup.js', 'jest-canvas-mock'],
   moduleFileExtensions: ['js', 'jsx'],
+  testMatch: ['**/__tests__/**/*.(test|spec).js?(x)'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',

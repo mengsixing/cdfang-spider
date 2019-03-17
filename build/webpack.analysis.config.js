b4c4aa43275e039716d4252db6b15b92196a7eff
yhlben <yinhengliben@gmail.com>
2019-03-17 08:53:48 +0800

    feat: add speed-measure-webpack-plugin

--- build/webpack.analysis.config.js
+++ build/webpack.analysis.config.js
@@ -1,6 +1,11 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+
 const config = require('./webpack.prod.config');
+
+// 检查loader，plugin的运行速度
+const smp = new SpeedMeasurePlugin();
 
 config.plugins.push(new BundleAnalyzerPlugin());
 
-module.exports = config;
+module.exports = smp.wrap(config);

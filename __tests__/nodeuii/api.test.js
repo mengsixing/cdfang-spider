6dcd9ba0d6ac0e98106864740948b394fc209113
yhlben <yinhengliben@gmail.com>
2019-03-24 14:33:05 +0800

    test: 修改 node 测试用例

--- __tests__/nodeuii/api.test.js
+++ __tests__/nodeuii/api.test.js
@@ -2,14 +2,15 @@
 import supertest from 'supertest';
 import app from '../../dist/app';
 
-describe('api 测试', () => {
+var server = app.listen();
 
+describe('api 测试', () => {
   function request(){
-    return supertest(app.listen());
+    return supertest(server);
   }
 
   it('api 返回数据格式是否正确？', (done) => {
-    request()
+    return request()
       .get('/getMongoData').then(response=>{
         const item1 = JSON.parse(response.text)[0];
         expect(item1).toHaveProperty('_id');
@@ -20,7 +21,8 @@ describe('api 测试', () => {
         expect(item1).toHaveProperty('endTime');
         expect(item1).toHaveProperty('status');
         done();
-      })
+        server.close();
+      });
   });
 
 });

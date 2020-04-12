67f10cd296cb22a47fbbe46c9c5dbc85e3af8c11
lmjben <yinhengliben@gmail.com>
2020-04-12 21:43:22 +0800

    test: 完善测试

---
+++ __tests__/setupTests.ts
@@ -0,0 +1,14 @@
+const customGlobal = global;
+Object.defineProperty(customGlobal, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
